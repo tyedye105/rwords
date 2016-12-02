8257fb049356d5b5b0dddfda6c0fe7a57e665ea5
Marty Kovach <tyedye105@yahoo.com>
2016-12-02 10:47:18 -0800

    add passing .all specs for Definition.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,11 +1,17 @@
 class Definition
 
+  @@def_entries = []
+
   define_method(:initialize) do |definition|
     @definition = definition
   end
 
   define_method(:define) do
     @definition
+  end
+
+  define_singleton_method(:all) do
+    @@def_entries
   end
 
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,5 +2,18 @@ require('rspec')
 require('definition')
 
 describe(Definition) do
-  describe('let you give it a definit')
+
+  describe('#define') do
+    it('let you give it a definition') do
+      test_def = Definition.new("add meaning to the word")
+      expect(test_def.define()).to(eq("add meaning to the word"))
+    end
+  end
+
+  describe('.all') do
+    it('will return an empty array at first') do
+      expect(Definition.all()).to(eq([]))
+    end 
+  end
+
 end
