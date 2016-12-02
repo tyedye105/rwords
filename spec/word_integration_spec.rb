30740c2db26ca0512247305c3e7af6f40f24c825
Marty Kovach <tyedye105@yahoo.com>
2016-12-02 13:10:01 -0800

    add passing integration spec for the route to add a word to the dictionary

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -10,3 +10,14 @@ describe("path to add word form", {:type => :feature}) do
     expect(page).to have_content("Enter a word:")
   end
 end
+
+describe("path to add a word to the dictionary", {:type => :feature}) do
+  it ("let the user add a word to the dictionary") do
+    visit('/')
+    click_link("add a word")
+    fill_in('word_entry', :with => 'box')
+    click_button('add it!')
+    expect(page).to have_content('box')
+
+  end
+end
